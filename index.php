b2b40b13dd3ade47be227a5b948b68803660dd9a
Anton Stashkevich <mirrodium@mail.ru>
2020-09-11 14:23:32 +0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+CModule::IncludeModule('iblock');?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Планировщик задач</title>
+</head>
+
+<body>
+</body>
+</html>
