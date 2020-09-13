85ef2471963df30a9fc9ec711e3cc362dd0899e0
Anton Stashkevich <mirrodium@mail.ru>
2020-09-13 12:46:39 +0700

    add class.php file

---
+++ class.php
@@ -0,0 +1,8 @@
+<?
+namespace Dcloud\TASKs;
+
+class Main{
+	
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-CModule::IncludeModule('iblock');?>
+CModule::IncludeModule('iblock');
+
+require_once('class.php');
+use Dcloud\TASKs\Main as Dcloud;
+
+Dcloud::test();
+
+/*<check and create DB>*/
+/*</check and create DB>*/?>
 <!doctype html>
 <html>
 <head>
