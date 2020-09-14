085bd1a3faa01f04935fb45b36b509ff94db16b2
Anton Stashkevich <mirrodium@mail.ru>
2020-09-14 10:33:01 +0700

    add js folder

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+global $USER;
 CModule::IncludeModule('iblock');
-
 require_once('class.php');
-use Dcloud\TASKs\Main as Dcloud;
-
-Dcloud::test();
-
-/*<check and create DB>*/
-/*</check and create DB>*/?>
+use Dcloud\TASKs\Main as Dcloud;?>
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Планировщик задач</title>
-</head>
+	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta charset="UTF-8">
+		<title>Планировщик задач</title>
+	</head>
 
-<body>
-</body>
+	<body>
+		<div class="container">
+			<?if(!$USER->isAuthorized()){?>
+				<font color="#f00">Вы не авторизированы!</font>
+			<?}else{?>
+				<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">111111</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">222222</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">33333</div>
+</div>
+			<?}?>
+		</div>
+	</body>
 </html>
